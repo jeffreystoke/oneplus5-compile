0ab7c3e50d72e16a72e171dc88f820ed8ca4fe72
jeffreystoke <jeffctor@gmail.com>
2017-12-12 23:22:44 +0800

    fix path error

--- 3-install-linux-headers.sh
+++ 3-install-linux-headers.sh
@@ -4,5 +4,5 @@ INSTALL_DIR=`pwd`/build/out
 
 mkdir -p $INSTALL_DIR
 pushd deps/kernel
-make ARCH=arm64 INSTALL_HDR_PATH=/usr/local/linux headers_install
+make ARCH=arm64 INSTALL_HDR_PATH=/data/local/linux headers_install
 popd
