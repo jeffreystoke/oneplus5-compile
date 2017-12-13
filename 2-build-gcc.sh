7765e3a0e43ade13a53fd5ef600958e31eee9edd
jeffreystoke <jeffctor@gmail.com>
2017-12-13 10:31:52 +0800

    remove official build flags for test

--- 2-build-gcc.sh
+++ 2-build-gcc.sh
@@ -10,23 +10,23 @@ pushd $BUILD_DIR
     --libdir=/data/local/lib \
     --target=aarch64-linux-android \
     --program-transform-name='s&^&aarch64-linux-android-&' \
-    --disable-multilib \
-    --enable-plugins \
-    --enable-libgomp \
-    --enable-gnu-indirect-function \
-    --enable-ld=default \
-    --enable-threads=posix \
-    --enable-eh-frame-hdr-for-static \
-    --enable-graphite=yes \
-    --enable-libstdcxx-time=yes \
-    --enable-clocale=gnu \
-    --enable-libmpx \
-    --enable-shared \
-    --enable-nls \
     --enable-languages=c,c++ \
-    --enable-cloog-backend=isl \
-    --enable-initfini-array \
+    --disable-multilib \
     --enable-gold
+    # --enable-plugins \
+    # --enable-libgomp \
+    # --enable-gnu-indirect-function \
+    # --enable-ld=default \
+    # --enable-threads=posix \
+    # --enable-eh-frame-hdr-for-static \
+    # --enable-graphite=yes \
+    # --enable-libstdcxx-time=yes \
+    # --enable-clocale=gnu \
+    # --enable-libmpx \
+    # --enable-shared \
+    # --enable-nls \
+    # --enable-cloog-backend=isl \
+    # --enable-initfini-array \
 
 make -j$(getconf _NPROCESSORS_ONLN) all-gcc
 make install-gcc
